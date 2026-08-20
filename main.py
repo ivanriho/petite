2b96cce913792572ad9e71bec9dac7dc354baf77
Ristic Hofrén <ivan.ristichofren@edu.vellinge.se>
2026-08-20 22:37:05 +0200

    yeah

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 print ("hello")
 print ("hello")
+print ("hello")
+if user == "admin":
+    print("Welcome, admin!")    
+
+if user == "guest":
+    print("Welcome, guest!")      
+
+
+    if ModuleNotFoundError:
+        print("Module not found. Please install the required module.")  
+        
